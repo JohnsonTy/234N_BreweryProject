c4f43994fa8dd02386d2e15c1f126d0406540333
JohnsonTy <102694942+JohnsonTy@users.noreply.github.com>
2023-05-09 14:41:16 -0700

    product updoot

--- ClassesLab_Core5/CustomerProductSolution/CustomerProductClasses/Product.cs
+++ ClassesLab_Core5/CustomerProductSolution/CustomerProductClasses/Product.cs
@@ -4,13 +4,16 @@ namespace CustomerProductClasses
 {
     public class Product
     {
+
         private int id;
         private string code;
         private string description;
         private decimal unitPrice;
         private int quantity;
 
-        public Product() { }
+        public Product()
+        {
+        }
 
         public Product(int productId, string productCode, string desc, decimal price, int qty)
         {
@@ -23,67 +26,39 @@ namespace CustomerProductClasses
 
         public int Id
         {
-            get
-            {
-                return id;
-            }
-            set
-            {
-                id = value;
-            }
+            get { return id; }
+            set { id = value; }
         }
 
         public string Code
         {
-            get
-            {
-                return code;
-            }
-            set
-            {
-                code = value;
-            }
+            get { return code; }
+            set { code = value; }
         }
 
         public string Description
         {
-            get
-            {
-                return description;
-            }
-            set
-            {
-                description = value;
-            }
+            get { return description; }
+            set { description = value; }
         }
 
         public decimal UnitPrice
         {
-            get
-            {
-                return unitPrice;
-            }
-            set
-            {
-                unitPrice = value;
-            }
+            get { return unitPrice; }
+            set { unitPrice = value; }
         }
 
         public int QuantityOnHand
         {
-            get
-            {
-                return quantity;
-            }
-            set
-            {
-                quantity = value;
-            }
+            get { return quantity; }
+            set { quantity = value; }
         }
 
         public override string ToString()
         {
-            return String.Format("Id: {0} Code: {1} Description: {2} UnitPrice: {3:C} Quantity: {4}", id, code, description, unitPrice, quantity);
+            return String.Format(
+                $"Id: {id} Code: {code} Description: {description} UnitPrice: ${unitPrice} Quantity: {quantity}");
         }
+        
     }
 }
